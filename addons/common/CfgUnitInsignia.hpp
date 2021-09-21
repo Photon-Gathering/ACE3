63625009696f334363704113ab548d1d2a46af49
Photon-Gathering <photonmailhere@protonmail.com>
2021-09-21 21:43:20 +0200

    Fetching Update from Main Branch

--- addons/common/CfgUnitInsignia.hpp
+++ addons/common/CfgUnitInsignia.hpp
@@ -12,4 +12,28 @@ class CfgUnitInsignia {
         texture = QPATHTOF(data\insignia_banana_ca.paa);
         textureVehicle = "";
     };
+    class InsigPat1
+  	{
+  		displayName = "Norsk Flagg 1";
+  		author = "Proxno";
+  		texture = QPATHTOF(data\n_flag_1_co.paa);
+  		material="\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+  		textureVehicle = "";
+  	};
+    class InsigPat2
+  	{
+  		displayName = "Norsk Flagg 2";
+  		author = "Proxno";
+  		texture = QPATHTOF(data\n_flag_2_co.paa);
+  		material="\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+  		textureVehicle = "";
+  	};
+    class InsigPat3
+  	{
+  		displayName = "330 Skvadron";
+  		author = "Proxno";
+  		texture = QPATHTOF(data\n_330sqdr_co);
+  		material="\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";
+  		textureVehicle = "";
+  	};
 };
